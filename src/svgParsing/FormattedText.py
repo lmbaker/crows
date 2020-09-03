42ce097b0688e983e2a7b9152a8720eb956c88b7
Lauren Baker <lbaker@appliedinvention.com>
2020-09-03 15:43:02 -0400

    pull text from SVG, working on spacing cases

---
+++ src/svgParsing/FormattedText.py
@@ -0,0 +1,241 @@
+import os
+import re
+import xml.etree.ElementTree as ET
+
+xml_namespace = {'svg': '{http://www.w3.org/2000/svg}'}
+svg_directory = '/home/lauren/svg'
+
+
+def parse_svg_tag(element):
+    '''Elements from SVG files have tags that look like
+    {http://www.w3.org/2000/svg}path, this method removes the curly braces and
+    their contents.'''
+    return re.sub('{.*}', '', element.tag)
+
+
+def traverse_element_tree(root_element):
+    '''For preorder traversal.
+
+    element: Element
+    '''
+    yield root_element
+    for child in root_element:
+        yield from traverse_element_tree(child)
+
+
+def print_children(element, depth):
+    '''For preorder traversal of an ElementTree, print the tag of each node,
+    using indentation for depth.'''
+    if parse_svg_tag(element) == 'tspan':
+        print(element.text)
+    print(' '*2*depth, parse_svg_tag(element))
+    for child in element:
+        print_children(child, depth+1)
+
+
+def svg_to_text(filename):
+    '''Given an SVG filename representing a page of the 5e SRD, return a list
+    of objects with text from the file.
+
+    filename: str
+    return: List[FormattedText]
+    '''
+    tree = ET.parse(os.path.join(svg_directory, filename))
+    root = tree.getroot()
+    top_g = root.find(xml_namespace['svg'] + 'g')
+
+    gen = traverse_element_tree(top_g)
+    text_list = []
+    for element in gen:
+        if parse_svg_tag(element) == 'text':
+            # The method to create a FormattedText object returns None if there
+            # is no text.
+            text_object = FormattedText.from_text_element(element)
+            if text_object:
+                text_list.append(text_object)
+
+    merged_text_list = []
+    merge_text = text_list[0]
+    for text in text_list[1:]:
+        if merge_text.should_merge(text):
+            merge_text = merge_text.merge(text)
+        else:
+            merged_text_list.append(merge_text)
+            merge_text = text
+    merged_text_list.append(merge_text)
+
+    # Strip the footer and page number
+    if ('Permission granted' in merged_text_list[0].text and
+            'System Reference Document' in merged_text_list[2].text):
+        if len(merged_text_list) == 2:
+            # In case there is a blank page
+            merged_text_list = []
+        merged_text_list = merged_text_list[3:]
+
+    # else:
+    #    raise ValueError('Page found with no footer, this is unexpected.')
+    return merged_text_list
+
+
+def merge_pages(page_1, page_2):
+    '''Combine two lists of FormattedText items. Check if the last element of
+    the first list should be combined with the first element of the second
+    list.
+
+    page_1: List[FormattedText]
+    page_2: List[FormattedText]
+    return: List[FormattedText]
+    '''
+    if not page_1 or not page_2:
+        merged = page_1 + page_2
+
+    elif page_1[-1].should_merge(page_2[0]):
+        # Protection if page 2 is a list with only one FormattedText item.
+        if len(page_2) == 1:
+            rest_of_page_2 = []
+        else:
+            rest_of_page_2 = page_2[1:]
+        merged = page_1[:-1] + [page_1[-1].merge(page_2[0])] + rest_of_page_2
+
+    else:
+        merged = page_1 + page_2
+
+    return merged
+
+
+class FormattedText:
+
+    def __init__(self, text, color, size, bold=False, italic=False):
+        '''Constructor
+
+        text: str
+        color: str representing a hex color
+        size: int font size
+        '''
+
+        self.text = text
+        self.color = color
+        self.size = size
+        self.bold = bold
+        self.italic = italic
+
+    @classmethod
+    def from_text_element(cls, element):
+        '''Turn an Element into a FormattedText object, which has the text and
+        various attributes for formatting details.
+
+        element: Element
+        return: FormattedText
+        '''
+        # Text style attributes look like:
+        # "font-variant:normal;font-weight:600;font-size:75px"
+        # font-weight can be bold or normal, font-style can be italic
+        if parse_svg_tag(element) != 'text':
+            raise ValueError(
+                "from_text_element received an element with tag" +
+                "'{}', should be 'text'".format(element.parse_svg_tag))
+
+        # Check children for a tspan element
+        child_element_tags = [parse_svg_tag(child) for child in element]
+        tspan_instances = child_element_tags.count('tspan')
+        if tspan_instances == 0:
+            return None
+        elif tspan_instances > 1:
+            print(('Unexpected parsing condition - text grouping has multiple '
+                   'tspan children.'))
+
+        tspan_element = list(element)[child_element_tags.index('tspan')]
+
+        # Harvest text style
+        style_dict = {}
+        for style_key_value in element.get('style').split(';'):
+            style_key, style_value = style_key_value.split(':')
+            style_dict[style_key] = style_value
+
+        clean_text = cls.fix_text(tspan_element.text)
+        text_color = style_dict['fill']  # a hex string preceeded by a '#'
+        font_size = int(style_dict['font-size'][:-2])  # an integer
+        text_bold = style_dict.get('font-weight') == 'bold'
+        text_italic = style_dict.get('font-style') == 'italic'
+
+        formatted_text = cls(clean_text,
+                             text_color,
+                             font_size,
+                             text_bold,
+                             text_italic)
+
+        return formatted_text
+
+    def should_merge(self, other_element):
+        '''Determine if these two text objects should be combined.
+
+        This method checks the font size and color and returns True if they
+        match.
+
+        other_element: Element
+        return: bool
+        '''
+        if (self.size == other_element.size and
+                self.color == other_element.color):
+            return True
+        else:
+            return False
+
+    def merge(self, other_element):
+        '''Return a new FormattedText instance with the text from self and
+        other_element.
+
+        Note: bold and italic formatting will be lost with the current
+        implementation of this method.
+
+        other_element: Element
+        return: FormattedText
+        '''
+
+        if (self.size != other_element.size or
+                self.color != other_element.color):
+            raise ValueError(
+                ("Cannot merge provided text '{}' and '{}', different font "
+                 "size or color.").format(self.text, other_element.text))
+
+        # Sometimes one piece of text ends with a word, and the next piece
+        # starts the next word, and there is no space in between. Other times,
+        # one piece of text ends midway through a word, and the next one
+        # finishes the word.
+        # Add a space between the two pieces to join if the first ends with a
+        # lowercase letter (and no newline) and the next starts with an
+        # uppercase letter.
+        if self.text == '' or other_element.text == '':
+            new_text = self.text + other_element.text
+        else:
+            end_of_first_text_is_alpha = (self.text[-1].isalpha() or
+                                          self.text[-1] == '-')
+            start_of_next_text_is_lowercase = (
+                (other_element.text[0].isalpha() and
+                 other_element.text[0].islower()) or
+                other_element.text[0] == '-')
+            if end_of_first_text_is_alpha and start_of_next_text_is_lowercase:
+                new_text = self.text + other_element.text
+            elif self.text[-1] == ' ' or other_element.text[0] == ' ':
+                new_text = self.text + other_element.text
+            elif self.text[-1] == '-' and other_element.text[0].isalpha():
+                new_text = self.text + other_element.text
+            else:
+                new_text = self.text + ' ' + other_element.text
+        return FormattedText(new_text, self.color, self.size, False, False)
+
+    @staticmethod
+    def fix_text(text):
+        '''Apply hard-coded rules to clean up text (address tabs and newlines)
+
+        text: str
+        return: str'''
+
+        if text == '\t':
+            return ''
+        else:
+            return text.replace('\t', ' ')
+
+    def __repr__(self):
+
+        return '<{}> <{}> {}'.format(self.color, self.size, repr(self.text))
---
+++ src/svgParsing/FormattedText_test.py
@@ -0,0 +1,72 @@
+from functools import partial
+import pytest
+
+from FormattedText import FormattedText
+
+no_space_cases = [('T', 'winned'),
+                  ('Half', '-'),
+                  ('-', 'Orc'),
+                  ('-', 'orc'),
+                  ('abc ', 'Abc'),
+                  ('abc', ' Abc'),
+                  ('Ins', 'ight'),
+                  ('10', '-'),
+                  ('G', 'M'),
+                  ('', 'x'),
+                  ('x', ''),
+                  ('x', '.'),
+                  ]
+
+space_cases = [('Story', 'Work'),
+               ('abc.', 'Abc'),
+               ('dice:', '1d8'),
+               ('1', 'word'),
+               ('word', '1'),
+               ]
+
+bold_word = FormattedText('bold', color='#000000', size=42, bold=True)
+bold_word_sp = FormattedText('bold ', color='#000000', size=42, bold=True)
+italic_word = FormattedText('italic', color='#000000', size=42, italic=True)
+plain_word = FormattedText('plain', color='#000000', size=42)
+
+special_no_space_cases = [(bold_word_sp, plain_word),
+                          (bold_word_sp, bold_word)]
+
+special_space_cases = [(bold_word, plain_word),
+                       (bold_word, italic_word),
+                       (bold_word, bold_word),
+                       (plain_word, bold_word),
+                       (italic_word, plain_word),
+                       ]
+
+# Helper function, in case the arguments to FormattedText change. Generates a
+# FormattedText instance where the only changing input is the text.
+formatted_text_fn = partial(FormattedText, color='#000000', size=42)
+
+
+@pytest.mark.parametrize("text_1, text_2", no_space_cases)
+def test_merge_no_space(text_1, text_2):
+    formatted_text_1 = formatted_text_fn(text_1)
+    formatted_text_2 = formatted_text_fn(text_2)
+    expected = text_1 + text_2
+    assert formatted_text_1.merge(formatted_text_2).text == expected
+
+
+@pytest.mark.parametrize("text_1, text_2", space_cases)
+def test_merge_add_space(text_1, text_2):
+    formatted_text_1 = formatted_text_fn(text_1)
+    formatted_text_2 = formatted_text_fn(text_2)
+    expected = text_1 + ' ' + text_2
+    assert formatted_text_1.merge(formatted_text_2).text == expected
+
+
+@pytest.mark.parametrize("form_text_1, form_text_2", special_no_space_cases)
+def test_merge_special_no_space(form_text_1, form_text_2):
+    expected = form_text_1.text + form_text_2.text
+    assert form_text_1.merge(form_text_2).text == expected
+
+
+@pytest.mark.parametrize("form_text_1, form_text_2", special_space_cases)
+def test_merge_special_space(form_text_1, form_text_2):
+    expected = form_text_1.text + ' ' + form_text_2.text
+    assert form_text_1.merge(form_text_2).text == expected
