6fd9259376e2e6256537a781dbde243e83808e25
Lauren Baker <lbaker@appliedinvention.com>
2020-12-15 18:40:34 -0500

    Changing configs to run predictions on AWS

--- src/evaluation/config.py
+++ src/evaluation/config.py
@@ -1,4 +1,5 @@
 #data_filepath = '/home/lauren/dndtep/srdAnswer/data/documents-dd2e8b2'
 #data_filepath = '/home/lauren/dndtep/srdAnswer/data/documents-487d96f'
-data_filepath = 'data/documents-5b3b0de'
+#data_filepath = 'data/documents-5b3b0de'
+data_filepath = 'data/generated'
 benchmark_filename = 'benchmark.json'
--- src/questionAnswering/config.py
+++ src/questionAnswering/config.py
@@ -5,5 +5,5 @@ generated_srd_filepath = os.path.join(data_filepath, 'srd_articles.json')
 
 # Tuple, first argument is the model name or filepath, and second argument
 # is a boolean for whether the model references a local file.
-model_name_or_path = ('models/roberta-base-squad2-v2', True)
-#model_name_or_path = ('deepset/roberta-base-squad2', False)
+#model_name_or_path = ('models/roberta-base-squad2-v2', True)
+model_name_or_path = ('deepset/roberta-base-squad2', False)
