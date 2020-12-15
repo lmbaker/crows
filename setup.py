27c7707a554ab21a150a889ae6ad9141d73ca860
Lauren Baker <lbaker@appliedinvention.com>
2020-12-15 18:42:49 -0500

    Add required packages to setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
 
-print(find_packages(where='src'))
-
 setup(name='srdAnswer',
       version='0.1',
       package_dir={'': 'src'},
       packages=find_packages(where='src'),
       scripts=['bin/generate_articles.py'],
-      #package_data={'capitalize': ['data/cap_data.txt']},
+      install_requires=[
+          'flask',
+          'farm-haystack',
+      ]
       )
